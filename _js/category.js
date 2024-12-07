48f6206f4609e1c14a143cbd2789746a6e1d275b
andersonmarioneto <netoanderson192@gmail.com>
2024-12-07 18:18:54 +0100

    07 de Dezembro

--- _js/category.js
+++ _js/category.js
@@ -1,19 +1,21 @@
 /* Js by: Anderson Mário Neto | UI Designer */
 
-function filterElements(category) {
-    const card = document.querySelectorAll(".sub-container-pro");
+function filterElements(category, event) {
+    const cards = document.querySelectorAll(".sub-container-pro");
     const buttons = document.querySelectorAll(".buttons button");
 
     // Remove a classe 'active' de todos os botões
     buttons.forEach(button => button.classList.remove('active'));
 
     // Adiciona a classe 'active' ao botão clicado
-    event.target.classList.add('active');
+    event.currentTarget.classList.add('active');
 
-    card.forEach((element) => {
-        element.classList.remove("show");
+    // Mostra ou oculta os elementos com base na categoria
+    cards.forEach(element => {
         if (category === "All" || element.classList.contains(category)) {
             element.classList.add("show");
+        } else {
+            element.classList.remove("show");
         }
     });
 }

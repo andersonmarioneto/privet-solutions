59bd5c414960c0a8dbfe80657b9d64b7546a0be4
andersonmarioneto <netoanderson192@gmail.com>
2024-12-08 17:31:54 +0100

    08 de Dezembro

--- _js/category.js
+++ _js/category.js
@@ -2,12 +2,13 @@
 
 function filterElements(category, event) {
     const cards = document.querySelectorAll(".sub-container-pro");
-    const buttons = document.querySelectorAll(".buttons button");
+    const links = document.querySelectorAll(".categories a");
 
     // Remove a classe 'active' de todos os botões
-    buttons.forEach(button => button.classList.remove('active'));
+    links.forEach(link => link.classList.remove('active'));
 
-    // Adiciona a classe 'active' ao botão clicado
+    // Adiciona a classe 'active' à âncora clicada
+    event.preventDefault(); // Evita o comportamento padrão do link
     event.currentTarget.classList.add('active');
 
     // Mostra ou oculta os elementos com base na categoria
